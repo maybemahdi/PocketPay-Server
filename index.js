8cd8544a414e38886247a987ca22f2334d0e3e11
Mahdi Hasan Mahi <mh7266391@gmail.com>
2024-07-16 13:22:50 +0600

    set up done

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
+
+const port = process.env.PORT || 5000;
+
+// middleware
+const corsOptions = {
+  origin: ["http://localhost:5173", "http://localhost:5174"],
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.send("Hello from PocketPay Server..");
+});
+
+app.listen(port, () => {
+  console.log(`PocketPay is running on port ${port}`);
+});
